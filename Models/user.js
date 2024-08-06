5c98fc1b22a0f02843bd36a64908bb5a279d52b2
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-08-06 17:46:59 +0530

    user model

--- Models/user.js
+++ Models/user.js
@@ -1,16 +1,26 @@
-// const mongoose = require("mongoose");
-// const userSchema = mongoose.Schema({
-//   username: {
-//     type: String,
-//     unique: true,
-//     required: true,
-//   },
-//   deviceNumber: {
-//     type: Number,
-//     unique: true,
-//     required: true,
-//   },
-// });
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password:{
+    type:String,
+    unique: true,
+    required: true,
+  },
+  email:{
+    type:String,
+    unique: true,
+    required: true,
+  },
+  phone:{
+    type:String,
+    unique: true,
+    required: true,
+  }
+});
 
-// const User = mongoose.model("User", userSchema);
-// module.exports = User;
+const User = mongoose.model("User", userSchema);
+module.exports = User;
